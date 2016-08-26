a09dcfc7e3a2794cf8a3c17dc61aa6b9b7f4047d
Gang Fan <gang.fan@uconn.edu>
2016-08-25 20:33:21 -0400

    Dynamic Template

--- client/app.component.ts
+++ client/app.component.ts
@@ -6,4 +6,31 @@ import template from './app.component.html';
   selector: 'app',
   template
 })
-export class AppComponent {}
+
+export class AppComponent {
+  constructor() {
+    this.carpools = [
+      {
+      	'Time' : '2016-9-10 3:00PM',
+      	'From' : 'Storrs',
+      	'To' : 'New York City',
+      	'Provider' : 'Tom',
+      	'Contact' : '8605228976'
+      },
+      {
+      	'Time' : '2016-9-11 5:00PM',
+      	'From' : 'Storrs',
+      	'To' : 'Boston',
+      	'Provider' : 'Jhon',
+      	'Contact' : '8605228977'
+      },
+      {
+      	'Time' : '2016-9-10 10:00AM',
+      	'From' : 'Storrs',
+      	'To' : 'Hartford',
+      	'Provider' : 'Emmy',
+      	'Contact' : '8605234976'
+      }
+    ];
+  }
+}
